be3caa386658b6b3e9ac7f8c5bad6a7de107e860
Daniel Sun <danielsun12345@gmail.com>
2020-03-21 03:19:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -87,12 +87,15 @@ class Student
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT 
+      SELECT *
       FROM students
-      
+      WHERE grade = 10
+      limit ?
+    SQL
     
-    
-    
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end
   end 
   
 end
