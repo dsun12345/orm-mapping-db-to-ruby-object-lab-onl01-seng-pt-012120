4d587be9cb97cb9bcf816e93cf2184ea3b89c84f
Daniel Sun <danielsun12345@gmail.com>
2020-03-21 03:22:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -98,4 +98,15 @@ class Student
     end 
   end 
   
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+      limit 1
+    SQL
+    
+    DB[:conn].execute(sql)[0]
+  end 
+  
 end
